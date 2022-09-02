ce9d20932faeafc2487e2853203c50cc3b406206
gmcdr <tharazo101@gmail.com>
2022-09-02 18:43:49 -0300

    Inicial Config

---
+++ db/conn.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize'); //Chamar modelo do require
+
+//Definir a conexção
+const sequelize = new Sequelize('thoughts', 'postgres', '', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+
+//Realizar conexção
+try{
+    sequelize.authenticate()
+    console.log('Connected')
+}catch(err){
+    console.log(`Error ${err}`)
+}
+
+//Exporta modulo
+module.exports = sequelize
---
+++ routes/toughtsRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const ToughtsController = require('../controllers/ToughtsController')
+
+
+router.get('/', ToughtsController.showToughts)
+
+module.exports =  router  
